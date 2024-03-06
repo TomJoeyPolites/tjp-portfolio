bfd4f76dc4555574b96b1486a177b95020afdbb8
Tom Joey Polites <54125540+TomJoeyPolites@users.noreply.github.com>
2024-03-06 13:05:24 +1100

    Update Navbar.jsx
    alt ref added to github and linkedin icons

--- src/components/common/Navbar/Navbar.jsx
+++ src/components/common/Navbar/Navbar.jsx
@@ -47,13 +47,13 @@ const Navbar = () => {
 						target="_blank"
 						rel="noreferrer"
 						href="https://github.com/TomJoeyPolites">
-						<FaGithub className={styles.icon} size="45" />
+						<FaGithub className={styles.icon} size="45" alt="github link" />
 					</a>
 					<a
 						target="_blank"
 						rel="noreferrer"
 						href="https://www.linkedin.com/in/tom-polites-418318156/">
-						<FaLinkedin className={styles.icon} size="45" />
+						<FaLinkedin className={styles.icon} size="45" alt="linkedIn link" />
 					</a>
 					<CiFileOn className={styles.icon} size="45" />
 				</div>
