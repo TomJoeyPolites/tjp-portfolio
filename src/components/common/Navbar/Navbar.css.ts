1bc6cd0c30713db08600c92750d59b0d19e7900a
Tom Joey Polites <54125540+TomJoeyPolites@users.noreply.github.com>
2024-03-07 11:31:27 +1100

    Update Navbar.css.ts

--- src/components/common/Navbar/Navbar.css.ts
+++ src/components/common/Navbar/Navbar.css.ts
@@ -11,7 +11,7 @@ export const navbar = style({
   justifyContent: "space-between",
   width: "100%",
   backgroundColor: vars.colors.complementary,
-  height: "100vh",
+  minHeight: "100vh",
   color: vars.colors.primary,
   '@media': {
     'screen and (max-width: 875px)': {
